6ade01b792fd7c959f99ce8fee5a9027595365a0
ysusuk <yurasusuk@gmail.com>
2015-08-14 13:05:22 +0200

    imports

--- src/main/scala/cook/book/package.scala
+++ src/main/scala/cook/book/package.scala
@@ -4,6 +4,7 @@ package cook
  * @author Yura.Susuk yurasusuk@gmail.com.
  */
 package object book {
+  import java.util.{HashMap => JavaHashMap, HashMap => _}
 
   implicit class StringExt1(val s: String) {
     def mew = "mew - mew!"
