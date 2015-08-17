fca3f49cffe87be96f2793f99dfb7c7b5311bb06
Yuriy Susuk <yurasusuk@gmail.com>
2015-08-17 14:19:49 +0200

    indexes method

--- src/main/scala/cook/book/collection/package.scala
+++ src/main/scala/cook/book/collection/package.scala
@@ -1,8 +1,41 @@
 package cook.book
 
+import scala.collection.mutable
+import scala.collection.mutable.{Map}
+import scala.collection.immutable
+
 /**
  * @author Yura.Susuk yurasusuk@gmail.com.
  */
 package object collection {
 
+  def indexesFold(in: String): immutable.Map[Char, immutable.TreeSet[Int]] = {
+    val inWithIndex = in.zipWithIndex
+    (immutable.Map[Char, immutable.TreeSet[Int]]() /: inWithIndex)(
+      (m: immutable.Map[Char, immutable.TreeSet[Int]], tup: (Char, Int)) => m + (tup._1 -> (m.getOrElse(tup._1, immutable.TreeSet[Int]()) + tup._2))
+    )
+  }
+
+  def indexes(in: String): immutable.Map[Char, immutable.TreeSet[Int]] = {
+    val inWithIndexes = in.zipWithIndex
+    inWithIndexes
+      .groupBy(_._1)
+      .map(tup => {
+      tup._1 -> (immutable.TreeSet[Int]() ++ tup._2.map(_._2).toSet)
+    })
+
+  }
+
+  def indexesM(in: String): Map[Char, mutable.TreeSet[Int]] = {
+    val map = Map[Char, mutable.TreeSet[Int]]()
+
+    val inWithIndexes = in.zipWithIndex
+    inWithIndexes foreach {
+      tup => map.getOrElseUpdate(tup._1, mutable.TreeSet()) += tup._2
+    }
+
+    map
+  }
 }
+
+
--- src/test/scala/IndexesSpec.scala
+++ src/test/scala/IndexesSpec.scala
@@ -1,6 +1,29 @@
+import org.specs2.Specification
+import cook.book.collection._
+
+import scala.collection.mutable
+
 /**
  * @author Yura.Susuk yurasusuk@gmail.com.
  */
-class IndexesSpec {
+class IndexesSpec extends Specification {
+  def is = s2"""
+
+    indexes of m should be {0}            $e1
+    indexes of m should be {0}            $e2
+
+  """
+
+  def e1 = {
+    val map = indexes("Mississippi")
+    map must contain('M' -> mutable.TreeSet(0))
+    map must contain('i' -> mutable.TreeSet(1, 4, 7, 10))
+  }
+
+  def e2 = {
+    val map = indexesFold("Mississippi")
+    map must contain('M' -> mutable.TreeSet(0))
+    map must contain('i' -> mutable.TreeSet(1, 4, 7, 10))
+  }
 
 }
